893582224652f1ae7c2de8c9020d313bf4b2267d
Jacob North <microversallaboratories@gmail.com>
2020-02-25 10:16:54 -0800

    Setup .asm file with code structure

---
+++ CS271_hw2_dog-years_remark.asm
@@ -0,0 +1,12 @@
+TITLE gamedev     (cs271_gamedev_final.asm)
+; Description:  This program gets the age of the user and calculates their age in dog years (age x 7).
+INCLUDE Irvine32.inc
+
+.data
+
+
+.code
+main PROC
+	
+main ENDP
+END main
