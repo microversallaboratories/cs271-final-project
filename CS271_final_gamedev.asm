50114c088b77901fbbf3005fa44e8da57a8e7d81
shinhoj01 <56090472+shinhoj01@users.noreply.github.com>
2020-03-02 10:37:26 -0800

    Project Setup
    Added folder including sln to make asm file able to assemble. Also modified the file to see if it builds. From now on, the commit will be done in individual branch.

--- CS271_final_gamedev.asm
+++ CS271_final_gamedev.asm
@@ -1,12 +1,30 @@
-TITLE gamedev     (cs271_gamedev_final.asm)
-; Description:  This program gets the age of the user and calculates their age in dog years (age x 7).
+TITLE gamedev     (CS271_final_gamedev.asm)
+
+; Author:
+; Course / Project ID                 Date:
+; Description:
+
 INCLUDE Irvine32.inc
+
+; (insert constant definitions here)
 
 .data
 
+; (insert variable definitions here)
+    ex BYTE "test 1 2 3.", 0
 
 .code
 main PROC
-	
+
+; (insert executable instructions here)
+    call Clrscr
+    mov EDX, OFFSET ex
+    call WriteString
+    call Crlf
+
+	exit	; exit to operating system
 main ENDP
+
+; (insert additional procedures here)
+
 END main
