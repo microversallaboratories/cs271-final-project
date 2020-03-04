f1ed178079a85b4f84b3697f8aab4455c64067d2
shinhoj01 <56090472+shinhoj01@users.noreply.github.com>
2020-03-03 19:51:35 -0800

    Can now read arrow key
    Read textbook for Win32 console application, and it is very helpful

--- CS271_final_gamedev.asm
+++ CS271_final_gamedev.asm
@@ -5,21 +5,32 @@ TITLE gamedev     (CS271_final_gamedev.asm)
 ; Description:
 
 INCLUDE Irvine32.inc
+INCLUDE Macros.inc
 
 
 .data
 
-    ex BYTE "test 1 2 3.", 0
-
 .code
 main PROC
-begin:
-    call Clrscr
-    mov EDX, OFFSET ex
-    call WriteString
-    call Crlf
 
-	exit	; exit to operating system
+L1: mov EAX, 10
+    call Delay
+    call ReadKey
+    jz L1
+
+    cmp dx, VK_RIGHT
+    jne L2
+    mWrite <"Right key is pressed", 0dh, 0ah>
+    jmp L4
+L2: 
+    cmp dx, VK_LEFT
+    jne L3
+    mWrite <"Left key is pressed", 0dh, 0ah>
+    jmp L4
+L3: 
+    mWrite <"Arrow key is not pressed", 0dh, 0ah>
+L4:
+    exit	; exit to operating system
 main ENDP
 
 
