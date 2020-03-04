0515cb06138ed66c922774a6dce8c3303bb9ece2
shinhoj01 <56090472+shinhoj01@users.noreply.github.com>
2020-03-03 20:36:55 -0800

    Key Input Part sorted

--- CS271_final_gamedev.asm
+++ CS271_final_gamedev.asm
@@ -13,24 +13,47 @@ INCLUDE Macros.inc
 .code
 main PROC
 
-L1: mov EAX, 10
-    call Delay
-    call ReadKey
-    jz L1
+KeyInput:
+    KeyInputLoop:
+        mov EAX, 10     ; Delay time
+        call Delay      ; Delay
+        call ReadKey    ; Read Key input
+        jz KeyInputLoop ; Jump back to KeyInputLoop if there is no key input
+    LeftKeyCheck:
+        cmp dx, VK_LEFT
+        jne UpKeyCheck
+        mWrite <"Left key is pressed", 0dh, 0ah>
+        jmp KeyInputEnd
+    UpKeyCheck:
+        cmp dx, VK_UP
+        jne RightKeyCheck
+        mWrite <"Up key is pressed", 0dh, 0ah>
+        jmp KeyInputEnd
+    RightKeyCheck:
+        cmp dx, VK_Right
+        jne DownKeyCheck
+        mWrite <"Right key is pressed", 0dh, 0ah>
+        jmp KeyInputEnd
+    DownKeyCheck:
+        cmp dx, VK_DOWN
+        jne EscapeKeyCheck
+        mWrite <"Down key is pressed", 0dh, 0ah>
+        jmp KeyInputEnd
+    EscapeKeyCheck:
+        cmp dx, VK_ESCAPE
+        jne OtherKeyPressed
+        mWrite <"ESCAPE key is pressed", 0dh, 0ah>
+        jmp TempExit
+    OtherKeyPressed: 
+        mWrite <"Other key is pressed", 0dh, 0ah>
+        jmp KeyInputEnd
 
-    cmp dx, VK_RIGHT
-    jne L2
-    mWrite <"Right key is pressed", 0dh, 0ah>
-    jmp L4
-L2: 
-    cmp dx, VK_LEFT
-    jne L3
-    mWrite <"Left key is pressed", 0dh, 0ah>
-    jmp L4
-L3: 
-    mWrite <"Arrow key is not pressed", 0dh, 0ah>
-L4:
+KeyInputEnd:
+    jmp KeyInput
+
+TempExit:
     exit	; exit to operating system
+
 main ENDP
 
 
